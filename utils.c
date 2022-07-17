fa2f54032c63462c5aa3ab613cdaefc5ed8ad26e
Tristan WAGNER <t.wagner66250@gmail.com>
2022-07-17 18:42:47 +0200

    added CQueue

--- utils.c
+++ utils.c
@@ -36,6 +36,14 @@ void* cAlloc(DWORD size) {
   return res;
 }
 
+// just an example on how to open a file
+void exampleFopen(){
+    FILE* f;
+    if (f = fopen("filepath", "rb")){
+        printf("fileSize: %d\n", fSize(f));
+    }
+}
+
 // return file size
 int fSize(FILE* f) {
     int res, original = ftell(f);
@@ -51,26 +59,44 @@ void Free(void* ptr) {
 
 // count character occurences in string
 DWORD strChOc(char* str, char ch) {
-    DWORD c = 0;
     if (!str) return 0;
+    DWORD c = 0;
     while (*str)
         if (*str++ == ch)
             c++;
     return c;
 }
 
+// count character occurences in string
+DWORD strLen(char* str) {
+    if (!str) return 0;
+    DWORD c = 0;
+    while (*str++)
+        c++;
+    return c;
+}
+
+char* itoa(int value) {
+    char buffer[256];
+    sprintf(buffer, "%d\0", value);
+    return buffer;
+}
+
 // return bit value at index of field
 int bit(int index, BYTE* field)
 {
   // get field to the byte containing the bit we are looking for
+  // we shift index 3 bits to the right to get the offset for the
+  // byte we want
   field += index >> 3;
-  // index cannot be more than 7 ?
+
+  // index of the bit in the field from 0 to 7
   index &= 7;
   return (*field & (1 << index)) ? 1 : 0;
 }
 
-// switch bit to 1 if it is 0
-int bitSwitch(int index, BYTE *field) {
+// set bit to 1 if it is 0
+int bitSet(int index, BYTE *field) {
   int res;
   field += index >> 3;
   index &= 7;
@@ -84,19 +110,149 @@ DWORD extractBitsFromBytes(BYTE* src, DWORD pos, DWORD bits) {
     DWORD i, res = 0;
     for (i = 0; i < bits; i++)
         if (bit(pos + i, src))
-            bitSwitch(i, (BYTE*) &res);
+         bitSet(i, (BYTE*) &res);
     return res;
 }
 
+// Queue (linked list)
+
+void QInit(void *head)
+{//Free entries in queue, not head.
+    ((CQueue *)head)->next=(CQueue *)head;
+    ((CQueue *)head)->last=(CQueue *)head;
+}
+
+void QInsert(void *entry, void *pred)
+{
+    CQueue *succ;
+    succ=((CQueue *)pred)->next;
+    ((CQueue *)entry)->last=(CQueue *)pred;
+    ((CQueue *)entry)->next=succ;
+    succ ->last=(CQueue *)entry;
+    ((CQueue *)pred)->next=(CQueue *)entry;
+}
+
+// insert at end
+void QPush(void *entry, void *q)
+{
+    QInsert(entry,((CQueue *) q)->last);
+}
+
+// insert at first
+void QUnshift(void *entry, void *q)
+{
+    QInsert(entry, q);
+}
+
+void QRemove(void *entry)
+{
+    ((CQueue *)entry)->last->next=((CQueue *)entry)->next;
+    ((CQueue *)entry)->next->last=((CQueue *)entry)->last;
+}
+
+void QWipe(void *head)
+{//Free entries in queue, not head.
+    CQueue *tmpq=((CQueue *)head)->next,*tmpq1;
+    while (tmpq!=head) {
+        tmpq1=((CQueue *)tmpq)->next;
+        QRemove(tmpq);
+        Free(tmpq);
+        tmpq=tmpq1;
+    }
+}
+
+int QCount(void *head)
+{//Count of nodes in queue, not head.
+    CQueue *tmpq=((CQueue *)head)->next;
+    int res=0;
+    while (tmpq!=head) {
+        res++;
+        tmpq=((CQueue *)tmpq)->next;
+    }
+    return res;
+}
+
+// for each element in queue, call callback function
+// passing pointer of current element
+void QForEach(void *head, QCallback cb) {
+    CQueue *ptr = ((CQueue *)head)->next;
+    while (ptr != head)
+    {
+        cb(ptr);
+        ptr = ptr->next;
+    }
+    
+}
+
+/*
+typedef struct Test Test;
+
+typedef struct Test {
+    Test *next, *last;
+    int value;
+} Test;
+
+void print(void *ptr){
+    Test *item = (Test *) ptr;
+    printf("value: %d\n", item->value);
+}
+
+void add(void *ptr){
+    Test *item = (Test *) ptr;
+    item->value += 1;
+}
+
 int main(){
-    char* s = "HELLLLLLLLLLLO";
+    char* s = "LHELLLLLLLLLLLO";
     printf("%s\n", s);
     printf("%d\n", strChOc(s, 'L'));
     printf("%d\n", bit(80, s));
-    bitSwitch(80, s);
+    bitSet(80, s);
     printf("%d\n", bit(80, s));
-    bitSwitch(80, s);
+    bitSet(80, s);
     printf("%d\n", bit(80, s));
     printf("%s\n", s);
     printf("%d\n", extractBitsFromBytes("AAA", 4, 4));
-}
+    printf("strlen %s: %d\n", s, strLen(s));
+    printf("strlen %s: %d\n", "hello", strLen("hello"));
+    int a = 3499995;
+    char* t = itoa(a);
+    printf("strlen %s: %d\n", t, strLen(t));
+
+    Test list;
+    QInit(&list);
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        Test *tmp = (Test *) malloc(sizeof(Test));
+        tmp->value = i;
+        QPush(tmp, &list);
+    }
+
+    Test *addHead = (Test *) malloc(sizeof(Test));
+    addHead->value = 10;
+    QUnshift(addHead, &list);
+
+    Test *ptr;
+    ptr = list.next;
+
+    QForEach(&list, print);
+
+    QForEach(&list, add);
+
+    QForEach(&list, print);
+
+    printf("List item count: %d\n", QCount(&list));
+        
+    QWipe(&list);
+
+    printf("List item count: %d\n", QCount(&list));
+
+    Test *add = (Test *) malloc(sizeof(Test));
+    add->value = 1;
+
+    QInsert(add, list.last);
+    
+    printf("List item count: %d\n", QCount(&list));
+
+} */
--- utils.h
+++ utils.h
@@ -11,4 +11,31 @@ typedef unsigned int DWORD;
 typedef unsigned char BOOL;
 
 extern int rand();
+extern int randmax(int max);
 extern void bubble_sort(int, int []);
+extern void* cAlloc(DWORD size);
+extern int fSize(FILE* f);
+extern void Free(void* ptr);
+extern DWORD strChOc(char* str, char ch);
+extern DWORD strLen(char* str);
+extern char* itoa(int value);
+extern int bit(int index, BYTE* field);
+extern int bitSet(int index, BYTE *field);
+extern DWORD extractBitsFromBytes(BYTE* src, DWORD pos, DWORD bits);
+
+typedef struct CQueue CQueue;
+typedef struct CQueue
+{
+    CQueue *next;
+    CQueue *last;
+} CQueue;
+typedef void (*QCallback)(void *);
+
+extern void QInit(void *head);
+extern void QInsert(void *entry, void *pred);
+extern void QPush(void *entry, void *q);
+extern void QUnshift(void *entry, void *q);
+extern void QRemove(void *entry);
+extern void QWipe(void *head);
+extern int QCount(void *head);
+extern void QForEach(void *head, QCallback cb);
