02fbf6041da2907bb57c6a2c1bcc5a3df7e8fce5
tristanwagner <t.wagner66250@gmail.com>
2023-07-24 02:09:05 +0200

    update platform

--- src/platform.h
+++ src/platform.h
@@ -1,15 +1,15 @@
 #if (defined __ANDROID__)
-#define PLATFORM "android"
+#define PLATFORM_ANDROID
 #elif defined(__APPLE__) || defined(__MACH__)
-#define PLATFORM "osx"
+#define PLATFORM_OSX
 #elif (defined linux || defined _linux || defined __linux__)
-#define PLATFORM "unix"
+#define PLATFORM_UNIX
 #elif defined(_WIN64) || defined(_WIN32)
 #define __USE_MINGW_ANSI_STDIO  1
 // Necessary windows defines before including windows.h, because it's retarded.
 #define OEMRESOURCE
 #include <windows.h>
-#define PLATFORM "win"
+#define PLATFORM_WIN
 #elif (defined __EMSCRIPTEN__)
-#define PLATFORM "web"
+#define PLATFORM_WEB
 #endif
